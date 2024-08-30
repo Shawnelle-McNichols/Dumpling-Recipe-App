824290795f6fab325e35f496705a8b08664fc73c
Shawnelle-McNichols <shawnellem101@gmail.com>
2024-08-29 20:03:34 -0400

    Update
    Basic Logic for Auth,Pantry and profile, needs fine tuning and designing

--- dumpling_client/app/index.tsx
+++ dumpling_client/app/index.tsx
@@ -1,22 +1,22 @@
-import { Text, View, Image, Pressable, TouchableOpacity} from "react-native";
+import { Text, View, Image, Pressable, TouchableOpacity } from "react-native";
 import styles from "../styles/styles";
 import { Link } from "expo-router";
 const logo = require("../assets/images/react-logo-2.png");
+const mascot = require("../assets/images/mascot.png")
 
 export default function Welcome() {
 
-  return (<>
-
-      <View style={styles.container}>
+  return (
+    <View style={styles.container}>
+      <Link href="/login">
         <Image source={logo} />
         <View >
-        <Link href="/signup" asChild><TouchableOpacity style={styles.btn_main}><Text style={styles.whitefont} >Signup</Text></TouchableOpacity></Link>
-        <Link href="/login" asChild><TouchableOpacity style={styles.btn_sec}><Text style={styles.colorfont} >Login</Text></TouchableOpacity></Link>
-      </View>
-      </View>
-     
-
-  </>
+          {/*<Link href="/signup" asChild><TouchableOpacity style={styles.btn_main}><Text style={styles.whitefont} >Signup</Text></TouchableOpacity></Link>
+        <Link href="/login" asChild><TouchableOpacity style={styles.btn_sec}><Text style={styles.colorfont} >Login</Text></TouchableOpacity></Link>*/}
+          <Image source={mascot} />
+        </View>
+      </Link>
+    </View>
   );
 }
 
--- dumpling_client/app/login.tsx
+++ dumpling_client/app/login.tsx
@@ -1,17 +1,28 @@
-import { Text, View, TextInput, TouchableOpacity, Image } from "react-native";
+import { Text, View, TextInput, TouchableOpacity, Image, Alert } from "react-native";
 import styles from "../styles/styles";
-import React, { useState } from "react";
-import { Link } from "expo-router";
+import React, {useState} from "react";
+import { Link, useRouter } from "expo-router";
+import { auth } from "../scripts/firebaseConfig.mjs";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const logo = require("../assets/images/Dumpling.png");
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const router = useRouter();
 
   const handleLogin = () => {
-    console.log('Email', email);
-    console.log('Password', password);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        Alert.alert("Login successful");
+        router.push("./(tabs)");
+      })
+      .catch((error) => {
+        Alert.alert("Email or password is incorrect", error.message)
+      });
+
   }
   return (
     <View style={styles.container}>
@@ -24,17 +35,20 @@ export default function Login() {
           style={styles.input}
           value={email}
           onChangeText={setEmail}
-          keyboardType="email-address" />
+          keyboardType="email-address"
+          autoCapitalize="none" />
       </View>
       <View style={styles.form_group}>
         <Text style={styles.label}>Password</Text>
         <TextInput style={styles.input}
           value={password}
-          onChangeText={setPassword} 
-          secureTextEntry/>
+          onChangeText={setPassword}
+          secureTextEntry />
       </View>
       <View style={styles.form_group}>
-        <TouchableOpacity style={styles.btn_main_md} onPress={handleLogin}><Text style={styles.whitefont}><Link href="/(tabs)">Login</Link></Text></TouchableOpacity>
+        <TouchableOpacity style={styles.btn_main_md} onPress={handleLogin}>
+          <Text style={styles.whitefont}>Login</Text>
+        </TouchableOpacity>
       </View>
 
     </View>
